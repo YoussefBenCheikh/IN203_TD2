30e394b311101f0e4913ddc4d552faac6da7d82c
YoussefBenCheikh <81634385+YoussefBenCheikh@users.noreply.github.com>
2021-11-16 19:10:56 +0100

    Create hyp_cub_dim3.cpp

---
+++ hyp_cub_dim3.cpp
@@ -0,0 +1,72 @@
+# include <chrono>
+# include <random>
+# include <cstdlib>
+# include <sstream>
+# include <string>
+# include <fstream>
+# include <iostream>
+# include <iomanip>
+# include <mpi.h>
+
+
+
+int main( int nargs, char* argv[] )
+{
+
+int rank, nbp, prev, next, tag1=1;
+
+	MPI_Request reqs[2]; // required variable for non-blocking calls
+	MPI_Status stats[2]; // required variable for Waitall routine
+
+	MPI_Init( &nargs, &argv );
+
+	MPI_Comm globComm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+
+	MPI_Comm_size(globComm, &nbp);
+
+	MPI_Comm_rank(globComm, &rank);
+
+if (rank==0) {
+	int jeton = 7;
+	MPI_Send(&jeton, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
+	MPI_Send(&jeton, 1, MPI_INT, 2, 1, MPI_COMM_WORLD);
+	MPI_Send(&jeton, 1, MPI_INT, 4, 1, MPI_COMM_WORLD);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==1) {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, stats);
+	MPI_Send(&jeton, 1, MPI_INT, 3, 1, MPI_COMM_WORLD);
+	MPI_Send(&jeton, 1, MPI_INT, 5, 1, MPI_COMM_WORLD);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==2){
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, stats);
+	MPI_Send(&jeton, 1, MPI_INT, 6, 1, MPI_COMM_WORLD);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==3) {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, stats);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==4) {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, stats);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==5) {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, stats);
+	MPI_Send(&jeton, 1, MPI_INT, 7, 1, MPI_COMM_WORLD);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else if (rank==6) {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 2, 1, MPI_COMM_WORLD, stats);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+} else {
+	int jeton;
+	MPI_Recv(&jeton, 1, MPI_INT, 5, 1, MPI_COMM_WORLD, stats);
+	std::cout<<"jeton: "<<jeton<<" affiché par tâche: "<<rank<<"\n";
+}
+
+MPI_Finalize();
+return EXIT_SUCCESS;
+}
